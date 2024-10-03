584b08e6d7b71e2af8eb1e600ca6ad78c5e96b9c
Jimi Joe <tano777864@gmail.com>
2024-10-03 09:07:44 -0400

    primera subirda

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\Api\studentController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/students', [studentController::class, 'index']);
+
+// Route::put('/students/{id}', function(){
+//     return "Actualizando estudiantes";
+// });
+Route::put('/students/{id}', [studentController::class, 'update']);
+Route::patch('/students/{id}', [studentController::class, 'updatePartial']);
+Route::post('/students', [studentController::class, 'store']);
+Route::get('/students/{id}', [studentController::class, 'show']);
+Route::delete('/students/{id}', [studentController::class, 'destroy']);
+
+
+// Route::delete('/students/{id}', function(){
+//     return "Eliminando estudiantes";
+// });
+
+// Route::get('/students/{id}', function(){
+//     return "Obteniendo un estudiante";
+// });
